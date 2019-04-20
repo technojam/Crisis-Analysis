09523956ddd3a75e6c3305bf67c5a6a44b39fb59
kaus2595 <kausr2595@gmail.com>
2019-04-20 18:00:27 +0530

    Minor changes

--- Plot.py
+++ Plot.py
@@ -7,26 +7,26 @@ data = pd.read_csv("K:\Projects\Dashboarding\seattle-crisis-data\crisis_data.csv
 data['ReportedDate'] = pd.to_datetime(data['ReportedDate'], format = "%d-%m-%Y")
 #print(data['Reported Date'])
 
-meets_by_month = data['ReportedDate'].groupby([data.ReportedDate.dt.year, data.ReportedDate.dt.month]).agg('count')
-#print(meets_by_month)
+crimes_by_month = data['ReportedDate'].groupby([data.ReportedDate.dt.year, data.ReportedDate.dt.month]).agg('count')
+#print()
 
-meets_by_month = meets_by_month.to_frame()
-#print(meets_by_month)
+crimes_by_month = crimes_by_month.to_frame()
+#print(crimes_by_month)
 
-meets_by_month['Date'] = meets_by_month.index
-#print(meets_by_month)
+crimes_by_month['Date'] = crimes_by_month.index
+#print(crimes_by_month)
 
-meets_by_month = meets_by_month.rename(columns = {meets_by_month.columns[0]:"crimes"})
-#print(meets_by_month['Date'])
+crimes_by_month = crimes_by_month.rename(columns = {crimes_by_month.columns[0]:"crimes"})
+#print(crimes_by_month['Date'])
 
-meets_by_month['Date'] = pd.to_datetime(meets_by_month['Date'], format = "(%Y, %m)")
-#print(meets_by_month['Date'])
+crimes_by_month['Date'] = pd.to_datetime(crimes_by_month['Date'], format = "(%Y, %m)")
+#print(crimes_by_month['Date'])
 
-meets_by_month = meets_by_month.reset_index(drop = True)
-#print(meets_by_month)
+crimes_by_month = crimes_by_month.reset_index(drop = True)
+#print(crimes_by_month)
 
-meets_by_month['month'] = meets_by_month.Date.dt.month
-#print(meets_by_month)
+crimes_by_month['month'] = crimes_by_month.Date.dt.month
+#print(crimes_by_month)
 
 """disposition_issued = data['Disposition'].value_counts().to_frame()
 #print(disposition_issued)
